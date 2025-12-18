a8fd8afb66327d51e09393df881b31c3532a4e0d
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-12-18 10:51:29 +0100

    Bonus 1 completed

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ function isActress(data: unknown): data is Actress {
   if (
     data &&
     typeof data === 'object' &&
+    data !== null &&
     "id" in data &&
     "name" in data &&
     "birth_year" in data &&
@@ -50,26 +51,83 @@ async function getActress(id: number): Promise<Actress | null> {
   return null
 }
 
-async function getAllActresses(): Promise<Actress[] | null> {
-  const res = await fetch(`http://localhost:3333/actresses`)
-    .then(res => res.json());
+async function getAllActresses(): Promise<Actress[]> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses`);
 
-  if (!Array.isArray(res)) return null;
+    if (!response.ok) {
+      throw new Error(`Errore nella richiesta: ${response.status}`);
+    }
 
-  const actresses = res.filter(isActress);
+    const data: unknown = await response.json();
 
-  return actresses;
+    if (!(data instanceof Array)) {
+      throw new Error('Formato dei dati non valido');
+    }
+
+    const actresses: Actress[] = data.filter(isActress);
+
+    return actresses;
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+    } else {
+      console.error('Errore sconosciuto');
+    }
+    return [];
+  }
 }
 
-async function getActresses(actressesIds: number[]): Promise<Actress[] | null> {
-  const promises = actressesIds.map(id => getActress(id));
-  const res = await Promise.all(promises);
-
-  const actresses = res.filter((act) => isActress(act));
-
-  return actresses.length ? actresses : null;
+async function getActresses(actressesIds: number[]): Promise<(Actress | null)[]> {
+  try {
+    const promises = actressesIds.map(id => getActress(id));
+    const response = await Promise.all(promises);
+    return response
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+    } else {
+      console.error('Errore sconosciuto');
+    }
+    return [];
+  }
 }
 
-console.log(getActress(1))
-console.log(getAllActresses())
-console.log(getActresses([1, 2, 3]))
+function createActress(actressObj: Omit<Actress, "id">): Actress {
+  return {
+    ...actressObj,
+    id: Math.floor(Math.random() * 10000)
+  }
+}
+
+function updateActress(actress: Actress, updates: Partial<Omit<Actress, "id" | "name">>): Actress {
+  return {
+    ...actress,
+    ...updates
+  }
+}
+
+
+getActress(1).then(actress => {
+  if (!actress) return;
+
+  const updated = updateActress(actress, {
+    biography: 'Michele Bozzuto'
+  });
+
+  console.log(updated);
+});
+
+getAllActresses().then(res => console.log(res));
+
+getActresses([1, 2, 3]).then(res => console.log(res));
+
+console.log(createActress({
+  name: 'Anne Hathaway',
+  birth_year: 1982,
+  biography: 'Anne Jacqueline Hathaway is an American actress. Her accolades include an Academy Award, a British Academy Film Award, a Golden Globe Award, and a Primetime Emmy Award.',
+  image: 'www.image.com',
+  most_famous_movies: ['Interstellar', 'The Devil Wears Prada', 'Batman'],
+  awards: 'Academy Awards',
+  nationality: 'American'
+}));
