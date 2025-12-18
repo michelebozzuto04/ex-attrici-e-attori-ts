269302dafe585422b53ed9e069a1219960b7ae4d
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-12-18 11:12:42 +0100

    Bonus 2 completed

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,40 @@ type Person = {
   image: string
 }
 
+type actressNationality =
+  | 'American'
+  | 'British'
+  | 'Australian'
+  | 'Israeli - American'
+  | 'South African'
+  | 'French'
+  | 'Indian'
+  | 'Israeli'
+  | 'Spanish'
+  | 'South Korean'
+  | 'Chinese'
+
+type actorNationality =
+  | 'Scottish'
+  | 'New Zealand'
+  | 'Hong Kong'
+  | 'German'
+  | 'Canadian'
+  | 'Irish'
+
 type Actress = Person & {
   most_famous_movies: [string, string, string]
   awards: string
-  nationality:
-  'American' |
-  'British' |
-  'Australian' |
-  'Israeli - American' |
-  'South African' |
-  'French' |
-  'Indian' |
-  'Israeli' |
-  'Spanish' |
-  'South Korean' |
-  'Chinese'
+  nationality: actressNationality
 }
 
+type Actor = Person & {
+  known_for: [string, string, string]
+  awards: [string, ...string[]]
+  nationality: actressNationality | actorNationality
+}
+
+// Actresses functions
 function isActress(data: unknown): data is Actress {
   if (
     data &&
@@ -107,6 +124,92 @@ function updateActress(actress: Actress, updates: Partial<Omit<Actress, "id" | "
   }
 }
 
+// Actors functions
+function isActor(data: unknown): data is Actor {
+  if (
+    data &&
+    typeof data === 'object' &&
+    data !== null &&
+    "id" in data &&
+    "name" in data &&
+    "birth_year" in data &&
+    "biography" in data &&
+    "image" in data &&
+    "known_for" in data &&
+    "awards" in data &&
+    "nationality" in data
+  ) {
+    return true
+  }
+  return false
+}
+
+async function getActor(id: number): Promise<Actor | null> {
+  const res = await fetch(`http://localhost:3333/actors/${id}`).then(res => res.json());
+  if (isActor(res)) {
+    return res as Actor
+  }
+  return null
+}
+
+async function getAllActors(): Promise<Actor[]> {
+  try {
+    const response = await fetch(`http://localhost:3333/actors`);
+
+    if (!response.ok) {
+      throw new Error(`Errore nella richiesta: ${response.status}`);
+    }
+
+    const data: unknown = await response.json();
+
+    if (!(data instanceof Array)) {
+      throw new Error('Formato dei dati non valido');
+    }
+
+    const actresses: Actor[] = data.filter(isActor);
+
+    return actresses;
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+    } else {
+      console.error('Errore sconosciuto');
+    }
+    return [];
+  }
+}
+
+async function getActors(actorsIds: number[]): Promise<(Actor | null)[]> {
+  try {
+    const promises = actorsIds.map(id => getActor(id));
+    const response = await Promise.all(promises);
+    return response
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+    } else {
+      console.error('Errore sconosciuto');
+    }
+    return [];
+  }
+}
+
+function createActor(actorObj: Omit<Actor, "id">): Actor {
+  return {
+    ...actorObj,
+    id: Math.floor(Math.random() * 10000)
+  }
+}
+
+function updateActor(actor: Actor, updates: Partial<Omit<Actor, "id" | "name">>): Actor {
+  return {
+    ...actor,
+    ...updates
+  }
+}
+
+
+// Actresses test
 
 getActress(1).then(actress => {
   if (!actress) return;
@@ -129,5 +232,31 @@ console.log(createActress({
   image: 'www.image.com',
   most_famous_movies: ['Interstellar', 'The Devil Wears Prada', 'Batman'],
   awards: 'Academy Awards',
+  nationality: 'American'
+}));
+
+// Actors test
+
+getActor(1).then(actor => {
+  if (!actor) return;
+
+  const updated = updateActor(actor, {
+    biography: 'Michele Bozzuto'
+  });
+
+  console.log(updated);
+});
+
+getAllActors().then(res => console.log(res));
+
+getActors([1, 2, 3]).then(res => console.log(res));
+
+console.log(createActor({
+  name: 'Christoph Waltz',
+  birth_year: 1956,
+  biography: "Christoph Waltz è un attore e regista austriaco con cittadinanza tedesca. È vincitore di vari riconoscimenti, compresi due premi Oscar, due premi Golden Globe, due premi BAFTA, due Critics' Choice Awards, due Screen Actors Guild Awards ed un Prix d'interprétation masculine al Festival di Cannes.",
+  image: 'www.image.com',
+  known_for: ['Inglorious Basterds', 'Django Unchained', 'Frankestein'],
+  awards: ['Academy Award', 'Golden Globe'],
   nationality: 'American'
 }));
